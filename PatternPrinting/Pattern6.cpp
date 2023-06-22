c3b4ab4bdcf5fd6d545c169e8e0e04e108f3d249
5 My name <115502017+pranavkangale5@users.noreply.github.com>
2023-06-22 20:57:38 +0530

    Pattern 2-10 solution files added

---
+++ PatternPrinting/Pattern6.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern6(int N)
+{
+  
+    for (int i = 0; i < N; i++)
+    {
+       
+        for (int j =N; j>i; j--)
+        {
+            cout <<N-j+1<<" ";
+        }
+       
+       
+        cout << endl;
+    }
+}
+
+int main()
+{   
+  
+    int N = 5;
+
+    pattern6(N);
+
+    return 0;
+}
